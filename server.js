e42803d16d39ef1cddf4d755bebc5c48ff2a0698
dsainvi <davcha1718@gmail.com>
2020-12-11 03:18:17 -0500

    please work

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-useNewUrlParser: true }) // useUnifiedTopology:true
+useNewUrlParser: true,  useUnifiedTopology:true
+})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
