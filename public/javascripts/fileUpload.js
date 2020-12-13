2097624d9a07e6220119b9febd5fa185b313ea7a
dsainvi <davcha1718@gmail.com>
2020-12-13 15:57:10 -0500

    Finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,4 +1,18 @@
-FilePond.registerPlugin(
+const rootStyles = window.getComputedStyle
+(document.documentElement)
+
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && 
+rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+  ready()
+} else { 
+  document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready() {
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+  const coverHeight = coverWidth / coverAspectRatio  
+  FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
@@ -11,3 +25,4 @@ FilePond.registerPlugin(
   })
   
   FilePond.parse(document.body);
+}
