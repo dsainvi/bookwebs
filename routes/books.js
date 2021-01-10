8a3bacfa28f2262aa07cc6b0ad8e75b52df11a9f
dsainvi <davcha1718@gmail.com>
2021-01-10 01:15:28 -0500

    blog

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
 const Author = require('../models/author')
+const Article = require('./../models/article')
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']//image
 
 // All Books Route
--- server.js
+++ server.js
@@ -11,25 +11,33 @@ const methodOverride = require('method-override')
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
+const articleRouter = require('./routes/articles')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-useNewUrlParser: true,  useUnifiedTopology:true
+useNewUrlParser: true,  useUnifiedTopology:true, useCreateIndex: true
 })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
+app.use('/articles', articleRouter)
+
+  
+  
 
 app.listen(process.env.PORT || 3000)
